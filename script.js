40e1e0ef5975ca790b16a9fa5d2997f6ffb81672
John <jlsmwilling@gmail.com>
2020-12-30 21:38:47 -0800

    completed
    completed expanding cards structure

---
+++ script.js
@@ -0,0 +1,20 @@
+// initilizing all DOM elements //
+
+const container = document.querySelector('.container');
+
+container.addEventListener('click', (e) => {
+  const panels = container.querySelectorAll('.panel');
+  const currentTarget = e.target.classList;
+
+  panels.forEach((element) => {
+    if (
+      element.classList.contains('active') !== currentTarget.contains('active')
+    ) {
+      element.classList.remove('active');
+    }
+  });
+
+  if (currentTarget.contains('panel')) {
+    currentTarget.add('active');
+  }
+});
